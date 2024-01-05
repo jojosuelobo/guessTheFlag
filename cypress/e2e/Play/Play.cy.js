77349083fa36cb080927deda4633204ce5f452ca
jojosuelobo <josuelobo2000@gmail.com>
2024-01-04 23:39:50 -0300

    🧪More e2e cases and start of hint test

--- cypress/e2e/Play/Play.cy.js
+++ cypress/e2e/Play/Play.cy.js
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
 describe('Game integration tests', () => {
-    let answer
     beforeEach(() => {
         cy.visit('')
         cy.startGameInHomepage('username')
@@ -11,22 +10,40 @@ describe('Game integration tests', () => {
         }).as('AnswerFetch');
     })
 
-    it('TESTE', () => {
-        cy.wait('@AnswerFetch').then((interception) => {
-            answer = interception.request.headers.answer
-            cy.log(answer)
-            cy.wait('@AnswerFetch').then((interception) => {
-                answer = interception.request.headers.answer
-                cy.log(answer)
-                cy.get('[name="inputCountry"]').type(`TESTE ${answer}`)
-            });
-        });
-
-        //cy.get('[name="inputCountry"]').type(answer)
-
-
-
+    it('gets a flag', () => {
+        cy.intercept('GET', '**flagcdn.com/**', {
+            statusCode: 200,
+        })
     });
 
+    it('get a point after type answer', () => {
+        let answer
+        cy.wait('@AnswerFetch').then((interception) => {
+            answer = interception.request.headers.answer
+            cy.wait('@AnswerFetch').then((interception) => {
+                answer = interception.request.headers.answer
+                cy.get('[name="inputCountry"]').type(`${answer}`, { delay: 0 })
+            });
+        });
+        cy.contains('Pontuação: 1').should('be.visible')
+    })
 
+    it('opens four hints after click HINT button', () => {
+        cy.get('[name="hint"]').click()
+        cy.get('.formsHint').should('exist').and('be.visible')
+        cy.get('[name="inputCountry"]').should('be.disabled')
+    });
+
+    it.only('get a point after guess the answer by hint', () => {
+        let answer
+        cy.wait('@AnswerFetch').then((interception) => {
+            answer = interception.request.headers.answer
+            cy.wait('@AnswerFetch').then((interception) => {
+                answer = interception.request.headers.answer
+                cy.get('[name="hint"]').click()
+
+            });
+        });
+        //cy.contains('Pontuação: 1').should('be.visible')
+    });
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,13 @@
 /// <reference types="cypress" />
 
+Cypress.Commands.overwrite('log', (subject, message) => cy.task('log', message));
+
 declare namespace Cypress {
     interface Chainable<Subject = any> {
         clickInJogarButton(): Chainable<any>;
         startGameInHomepage(username: string): Chainable<any>;
+        guessTheFirstFlagCorrectly(): Chainable<any>;
+        guessTheFlagCorrectly(): Chainable<any>;
     }
 }
 
@@ -15,9 +19,18 @@ Cypress.Commands.add('startGameInHomepage', (
     username: string = 'username'
 ) => {
     cy.clickInJogarButton()
-    cy.get('[name="usernameInput"]').type(username, {delay: 0})
+    cy.get('[name="usernameInput"]').type(username, { delay: 0 })
     cy.get('[name="start"]').click()
 })
 
-Cypress.Commands.overwrite('log', (subject, message) => cy.task('log', message));
+Cypress.Commands.add('guessTheFlagCorrectly', () => {
+    cy.intercept({
+        method: 'GET',
+        url: '**/gameAwnser',
+    }).as('AnswerFetch');
 
+    cy.wait('@AnswerFetch').then((interception) => {
+        const answer = interception.request.headers.answer
+        cy.get('[name="inputCountry"]').should('be.visible').clear().type(`${answer}`, { delay: 0 })
+    });
+})
