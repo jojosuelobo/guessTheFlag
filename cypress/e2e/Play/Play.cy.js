26a91735d7ff71169cb6f62ef524fd1b0fa26fa8
jojosuelobo <josuelobo2000@gmail.com>
2024-01-05 16:30:19 -0300

    🧪Incorret anwser e2e test

--- cypress/e2e/Play/Play.cy.js
+++ cypress/e2e/Play/Play.cy.js
@@ -9,13 +9,11 @@ describe('Game integration tests', () => {
             url: '**/gameAwnser',
         }).as('AnswerFetch');
     })
-
     it('gets a flag', () => {
         cy.intercept('GET', '**flagcdn.com/**', {
             statusCode: 200,
         })
     });
-
     it('get a point after type answer', () => {
         let answer
         cy.wait('@AnswerFetch').then((interception) => {
@@ -27,14 +25,24 @@ describe('Game integration tests', () => {
         });
         cy.contains('Pontuação: 1').should('be.visible')
     })
-
     it('opens four hints after click HINT button', () => {
         cy.get('[name="hint"]').click()
         cy.get('.formsHint').should('exist').and('be.visible')
         cy.get('[name="inputCountry"]').should('be.disabled')
     });
-
-    it.only('get a point after guess the answer by hint', () => {
+    it('get a point after guess the answer by hint', () => {
+        let answer
+        cy.wait('@AnswerFetch').then((interception) => {
+            answer = interception.request.headers.answer
+            cy.wait('@AnswerFetch').then((interception) => {
+                answer = interception.request.headers.answer
+                cy.get('[name="hint"]').click()
+                cy.contains(`${answer}`).should('exist').click()
+            });
+        });
+        cy.contains('Pontuação: 1').should('be.visible')
+    });
+    it('give the correct answer after guessing by hint', () => {
         let answer
         cy.wait('@AnswerFetch').then((interception) => {
             answer = interception.request.headers.answer
@@ -42,8 +50,27 @@ describe('Game integration tests', () => {
                 answer = interception.request.headers.answer
                 cy.get('[name="hint"]').click()
 
+                cy.contains(`${answer}`).click()
+                cy.get('.formsHint button').contains(`${answer}`).should('have.css', 'background-color', 'rgb(0, 128, 0)')
             });
         });
-        //cy.contains('Pontuação: 1').should('be.visible')
-    });
+    })
+    it.only('give the INCORRECT answer after guessing by hint', () => {
+        let answer
+        cy.wait('@AnswerFetch').then((interception) => {
+            answer = interception.request.headers.answer
+            cy.wait('@AnswerFetch').then((interception) => {
+                answer = interception.request.headers.answer
+                cy.get('[name="hint"]').click()
+
+                // cy.contains(`${answer}`).click()
+                // cy.get('.formsHint button').contains(`${answer}`).should('have.css', 'background-color', 'rgb(0, 128, 0)')
+                cy.get('.formsHint button').then((buttons) => {
+                    const correctButton = buttons.filter(`:contains("${answer}")`);
+                    const incorrectButton = buttons.not(correctButton);
+                    cy.wrap(incorrectButton).first().click().should('have.css', 'background-color', 'rgb(255, 0, 0)')
+                });
+            });
+        });
+    })
 })
